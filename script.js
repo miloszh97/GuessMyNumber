1d3807aa67984bbea26225b2c39a9ca37ea5c3c5
Milosz Harchut <miloszharchut97@gmail.com>
2023-04-28 10:26:17 +0100

    Refactored code

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('body').style.backgroundColor = '#222';
